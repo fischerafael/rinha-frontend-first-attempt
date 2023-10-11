55e368f4ab9365ccab3974a9444ed3494e5cd39b
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-10-11 17:46:17 -0300

    feat: initial commit

---
+++ client/pages/viewer/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import verySmallJson from "../../data/verysmall.json";
+import alltypes from "../../data/alltypes.json";
+
+export const PageViewer = () => {
+  return <JSONRender json={alltypes} />;
+};
+
+const JSONRender = ({ json }: { json: any }) => {
+  const renderJSON = (json: any) => {
+    if (typeof json === "object" && json !== null) {
+      return (
+        <ul>
+          {Object.keys(json).map((key, index) => (
+            <li key={index}>
+              <strong>{key}:</strong> {renderJSON(json[key])}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+    if (Array.isArray(json)) {
+      return (
+        <ul>
+          {json.map((item, index) => (
+            <li key={index}>{renderJSON(item)}</li>
+          ))}
+        </ul>
+      );
+    }
+    return <span>{json}</span>;
+  };
+
+  return <div className="json-tree-viewer">{renderJSON(json)}</div>;
+};
